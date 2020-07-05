8dc13bf723e82385b6295df7169afd94433ccc9f
Aaron Angelo Vicuna <aaron_vicuna@sutd.edu.sg>
2020-07-05 20:42:34 +0800

    Added events schema

---
+++ database/migrations/2020_07_05_023654_create_events_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('synopsis');
+            $table->string('excerpt');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->text('pre_booking_display_message');
+            $table->text('post_booking_display_message');
+            $table->boolean('social_show_facebook');
+            $table->boolean('social_show_linkedin');
+            $table->boolean('social_show_twitter');
+            $table->boolean('social_show_whatsapp');
+            $table->boolean('social_show_email');
+            $table->boolean('is_public');
+            $table->boolean('is_approved');
+            $table->boolean('for_approval');
+            $table->string('barcode_type');
+            $table->integer('checkout_timeout');
+            $table->bigInteger('department_id')->unsigned();
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('edited_by')->unsigned();
+            $table->bigInteger('venue_id')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('edited_by')->references('id')->on('users');
+            $table->foreign('venue_id')->references('id')->on('venues');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}

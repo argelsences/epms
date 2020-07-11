8405484f1fddbee8cbf0bc8e2e74a34822595b3d
Aaron Angelo Vicuna <aaron_vicuna@sutd.edu.sg>
2020-07-11 18:28:22 +0800

    Added foreign key from department and roles for users, added traits for roles in User model

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, SoftDeletes;
+    use HasApiTokens, Notifiable, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.

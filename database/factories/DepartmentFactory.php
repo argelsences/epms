1122c87a2ed8a57e7708b7914427ef35541091bd
Aaron Angelo Vicuna <aaron_vicuna@sutd.edu.sg>
2020-07-18 23:39:21 +0800

    Faker for User and Department

--- database/factories/DepartmentFactory.php
+++ database/factories/DepartmentFactory.php
@@ -7,6 +7,19 @@ use Faker\Generator as Faker;
 
 $factory->define(Department::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->company,
+        'email' => $faker->unique()->email,
+        'phone' => $faker->phoneNumber,
+        'facebook' => $faker->url,
+        'instagram' => $faker->url,
+        'logo_path' => '',
+        'page_header_bg_color' => $faker->hexColor,
+        'page_bg_color' => $faker->hexColor,
+        'page_text_color' => $faker->hexColor,
+        'google_analytics_code' => '',
+        'google_tag_manager_code' => '',
+        'url' => $faker->unique()->url,
+        'created_at' => now(),
+        'updated_at' => now(),
     ];
 });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@material.com',
+        $user = DB::table('users')->insert([
+            'name' => 'Aaron Angelo Vicuna',
+            'email' => 'arjieangelsences@gmail.com',
             'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
+            'password' => bcrypt('logan9c20'),
+            'designation' => 'Senior Specialist',
+            'department_id' => 1,
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => 6,
+            'model_type' => 'App\User',
+            'model_id' => 1
+        ]);
+
+        // make a role permission factory first
+        factory(App\User::class, 50)->create()->each(function($u) {
+            //$u->posts()->save(factory(App\Post::class)->make());
+            $roles = \Spatie\Permission\Models\Role::all();
+            $roleArray = $roles->pluck('name');
+            $the_role = $roleArray([range(0, count($roleArray) - 1)]);
+            $u->assignRole($the_role);
+        });
     }
 }

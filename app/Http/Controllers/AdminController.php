7aacd35066dd0a1f7002cd85185fdae2863475ad
Aaron Angelo Vicuna <aaron_vicuna@sutd.edu.sg>
2020-07-20 21:14:13 +0800

    Route changes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,7 @@ class AdminController extends Controller
 {
     //
     public function dashboard(){
-        return view('admin.dashboard');
+        //return view('admin.dashboard');
+        return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,19 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Auth::routes();
+
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::middleware('auth')->group(function(){
-    Route::get('/web-admin', 'AdminController@dashboard')->name('admin.dashboard');
-});
+Route::prefix('web-admin')->middleware('auth')->group(function(){
+	Auth::routes();
+	Route::get('/', 'AdminController@dashboard')->name('dashboard');
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 
-Route::group(['middleware' => ['role:super-administrator|administrator']], function () {
-    Route::get('/web-admin/users', 'UserController@index')->name('users.index');
-	Route::get('/web-admin/users/create', 'UserController@create')->name('users.create');
-	Route::post('/web-admin/users/store', 'UserController@store')->name('users.store');
-	Route::delete('/web-admin/users/{user}', 'UserController@destroy')->name('users.destroy');
-	Route::get('/web-admin/users/{user}/edit', 'UserController@edit')->name('users.edit');
-	Route::put('/web-admin/users/{user}', 'UserController@update')->name('users.update');
-});
-
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-
-Route::group(['middleware' => 'auth'], function () {
+	// remove later, for demo only
 	Route::get('table-list', function () {
 		return view('pages.table_list');
 	})->name('table');
@@ -64,12 +57,36 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('upgrade', function () {
 		return view('pages.upgrade');
 	})->name('upgrade');
+	// remove later for demo only
 });
 
+Route::group(['prefix' => 'web-admin','middleware' => ['role:super-administrator|administrator']], function () {
+    Route::get('users', 'UserController@index')->name('users.index');
+	Route::get('users/create', 'UserController@create')->name('users.create');
+	Route::post('users/store', 'UserController@store')->name('users.store');
+	Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+	Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
+	Route::put('users/{user}', 'UserController@update')->name('users.update');
+});
+
+/////Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+	
+});
+
+/*
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 });
+*/
+
+/*
+Route::namespace('Admin')->group(function () {
+    // Controllers Within The "App\Http\Controllers\Admin" Namespace
+});
+*/
 

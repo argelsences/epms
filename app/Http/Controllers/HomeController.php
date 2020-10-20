d1630b91b111169f400753f0da2aeb686594d22e
argelsences <aaron_vicuna@sutd.edu.sg>
2020-10-20 12:12:52 +0800

    script for event

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,47 @@ class HomeController extends Controller
         $department = Department::where('url', $URI)->firstOrFail();
         //dd($department);
         /////return view('');
+
+        /// script
+        /** @var Organiser $organiser */
+        $organiser = Organiser::findOrFail($organiser_id);
+
+        if (!$organiser->enable_organiser_page && !Utils::userOwns($organiser)) {
+            abort(404);
+        }
+
+        /*
+         * If we are previewing styles from the backend we set them here.
+         */
+        if ($request->get('preview_styles') && Auth::check()) {
+            $query_string = rawurldecode($request->get('preview_styles'));
+            parse_str($query_string, $preview_styles);
+
+            $organiser->page_bg_color = $preview_styles['page_bg_color'];
+            $organiser->page_header_bg_color = $preview_styles['page_header_bg_color'];
+            $organiser->page_text_color = $preview_styles['page_text_color'];
+        }
+
+        $upcoming_events = $organiser->events()->where([
+            ['end_date', '>=', now()],
+            ['is_live', 1]
+        ])->get();
+
+        $past_events = $organiser->events()->where([
+            ['end_date', '<', now()],
+            ['is_live', 1]
+        ])->limit(10)->get();
+
+        $data = [
+            'organiser'       => $organiser,
+            'tickets'         => $organiser->events()->orderBy('created_at', 'desc')->get(),
+            'is_embedded'     => 0,
+            'upcoming_events' => $upcoming_events,
+            'past_events'     => $past_events,
+        ];
+        /// end script
         
         return view('front.department.homepage', compact('department'));
     }
+    
 }

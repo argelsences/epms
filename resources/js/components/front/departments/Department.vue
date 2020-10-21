8968a84b3d47b8b41fc4f6586b5ffff2928a3f93
argelsences <aaron_vicuna@sutd.edu.sg>
2020-10-21 16:21:33 +0800

    improvement to homepage

--- resources/js/components/front/departments/Department.vue
+++ resources/js/components/front/departments/Department.vue
@@ -3,61 +3,29 @@
         <v-divider></v-divider>
         <div class="text-h4 text-center">Departments</div>
         <v-divider></v-divider>
-        <v-container>
-            <v-row justify="space-around">
-                <v-card width="400">
-                    <v-img height="200px" src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg">
-                    <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                        <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
-                        <v-toolbar-title class="title white--text pl-0">
-                            Messages
-                        </v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-btn color="white" icon>
-                        <v-icon>mdi-dots-vertical</v-icon>
-                        </v-btn>
-                    </v-app-bar>
+        <v-card class="mx-auto" max-width="400" v-for="row in rows" :key="row.id">
+            <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-card-title>{{row.name}}</v-card-title>
+            </v-img>
 
-                    <v-card-title class="white--text mt-8">
-                        <v-avatar size="56">
-                        <img
-                            alt="user"
-                            src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
-                        >
-                        </v-avatar>
-                        <p class="ml-3">
-                        John Doe
-                        </p>
-                    </v-card-title>
-                    </v-img>
+            <v-card-subtitle class="pb-0">
+                Number 10
+            </v-card-subtitle>
 
-                    <v-card-text>
-                    <div class="font-weight-bold ml-8 mb-2">
-                        Today
-                    </div>
+            <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
+                <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
 
-                    <v-timeline
-                        align-top
-                        dense
-                    >
-                        <v-timeline-item
-                        v-for="message in messages"
-                        :key="message.time"
-                        :color="message.color"
-                        small
-                        >
-                        <div>
-                            <div class="font-weight-normal">
-                            <strong>{{ message.from }}</strong> @{{ message.time }}
-                            </div>
-                            <div>{{ message.message }}</div>
-                        </div>
-                        </v-timeline-item>
-                    </v-timeline>
-                    </v-card-text>
-                </v-card>
-            </v-row>
-        </v-container>
+            <v-card-actions>
+            <v-btn color="orange" text>
+                Share
+            </v-btn>
+            <v-btn color="orange" text>
+                Explore
+            </v-btn>
+            </v-card-actions>
+        </v-card>
     </v-app>
 </template>
 <script>
@@ -68,26 +36,6 @@ export default {
         data() {
             return {
                 rows: [],
-                messages: [
-                    {
-                    from: 'You',
-                    message: `Sure, I'll see you later.`,
-                    time: '10:42am',
-                    color: 'deep-purple lighten-1',
-                    },
-                    {
-                    from: 'John Doe',
-                    message: 'Yeah, sure. Does 1:00pm work?',
-                    time: '10:37am',
-                    color: 'green',
-                    },
-                    {
-                    from: 'You',
-                    message: 'Did you still want to grab lunch today?',
-                    time: '9:47am',
-                    color: 'deep-purple lighten-1',
-                    },
-                ],
             }
         },
         computed: {},

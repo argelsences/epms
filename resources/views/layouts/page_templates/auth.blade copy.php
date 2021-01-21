8bd331223223188ce1214f233277a30331692a4d
argelsences <aaron_vicuna@sutd.edu.sg>
2021-01-21 18:27:23 +0800

    removed trash files

--- resources/views/layouts/page_templates/auth.blade copy.php
+++
@@ -1,8 +0,0 @@
-<div class="wrapper ">
-  <admin-menu></admin-menu>
-  <div class="main-panel">
-    @include('layouts.navbars.navs.auth')
-    @yield('content')
-    @include('layouts.footers.auth')
-  </div>
-</div>
